627596079bd02f93048669efbd3d4377d01a3142
Marco Nardone <28734811+majinmarco@users.noreply.github.com>
2024-02-26 16:17:48 -0400

    OpenAI only; not allowed to upload your own file for now because it is not working correctly.

--- PANDAS_Agent/data_qa.py
+++ PANDAS_Agent/data_qa.py
@@ -9,7 +9,7 @@ from langchain_experimental.tools.python.tool import PythonREPLTool
 from langchain.agents.agent_types import AgentType # for agent type assignment
 from langchain.agents.react.agent import create_react_agent
 from langchain.agents import (AgentExecutor, Tool) # agent executor to execute agent Tool, to make tool out of agent, ConversationalAgent to create template
-# from langchain.memory import ConversationBufferMemory # chat memory for agentfrom langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory # chat memory for agentfrom langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.prompts import PromptTemplate
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
 <<<<<<< HEAD
@@ -27,10 +27,10 @@ import matplotlib.pyplot as plt
 from langchain_community.callbacks import StreamlitCallbackHandler
 from langchain.callbacks.base import BaseCallbackManager
 from langchain.globals import set_verbose, set_debug
-from langchain_core.output_parsers import JsonOutputParser
 
 from langchain_community.llms import Ollama
-from langchain_google_vertexai import VertexAI
+from langchain_community.chat_models import ChatOllama
+from langchain_google_vertexai import VertexAI, ChatVertexAI, VertexAIModelGarden
 import google.auth
 
 from dotenv import load_dotenv
@@ -287,27 +287,27 @@ def agent_init( _llm, data_description, _file):
         Thought: I now know the final answer
         Final Answer: the final answer to the original input question
 
-        Output should be formatted as such:
-
-        {format_instructions}
-        
-        Do not transform the format above. It is your way of thinking through the problem.
-
-        Finally, please review all columns and groups of the dataset as context for the question before you start answering it so you understand what the user is referring to.
-
         **CONTEXT**
-        Here is the data description for context:
+        Here is the dataframe head for context:
         """+
-        data_description
+        
+        str(df.head())
+
         +"""
 
+        **CHAT HISTORY**
+
         Chat History:
-        {}
+        {chat_history}
+
+        **FINAL OUTPUT FORMATTING**
+        {format_instructions}
 
         **QUESTION**
 
         Question: {input} 
-        Thought: {agent_scratchpad}""",
+        Thought: {agent_scratchpad}
+        Action:""",
         partial_variables={"format_instructions":output_parser.get_format_instructions()},
     )
 
@@ -414,20 +414,29 @@ with st.spinner("🦾 Loading agent..."):
 run = False
 
 with st.sidebar:
+    st.markdown("Please select your model and type")
     # get params for functions
-    model_name = st.selectbox("Select model", ["gemini", "mistral", "openai"])
-    st.session_state["file"] = st.file_uploader("Upload a csv file", type=["csv"])
-
-    st.session_state["description"] = st.text_area("Description")
-    st.markdown("Dataset descirption should include a general description (e.g., the purpose of the data, what it is used for, etc.)")
+    model_name = st.selectbox("Select model", ["openai"])
+    model_category = st.selectbox("Select model type", ["static", "chat"])
+    st.warning("Because of conflicts with streamlit, you must set your file path in the code.")
 
     # initialize model, db, agent on run
     if st.button(label="Run", help="This will initialize the model, database, and agent. It will also reset the chat interface."):
-        st.session_state["run"] = True
+        with st.spinner("🚀 Loading model..."):
+            st.session_state["llm"] = model_init(model_name, model_category)
+            print(f"Switched to: {model_name}, {model_category}")
+
+        with st.spinner("🦾 Loading agent..."):
+            st.session_state["agent_chain"], st.session_state["output_parser"] = agent_init(st.session_state["llm"])
+
+        if "messages" in st.session_state:
+            st.session_state.messages = [
+            {"role": "assistant", "content": "Welcome to the Data QA bot! Ask whatever questions you'd like!"}
+        ]
 
 # Create a header element
 st.header("Dataset Q&A")
-st.markdown("**PLEASE RUN SIDEBAR FIRST BEFORE RUNNING CHAT INTERFACE**")
+st.markdown("**PLEASE RUN SIDEBAR FIRST BEFORE RUNNING CHAT INTERFACE**")    
 
 if "run" in st.session_state and st.session_state["run"] == True:
     with st.spinner("🚀 Loading model..."):
@@ -514,18 +523,18 @@ if user_input := st.chat_input("Question", key="input"):
             # Add the response to messages session state
             try:
                 st.session_state.messages.append(
-                    {"role": "assistant", "content":response["output"]["Final Answer"]})
+                    {"role": "assistant", "content":json.loads(response["output"])[0]["Final Answer"]})
             except:
                 st.session_state.messages.append(
                     {"role": "assistant", "content": response["output"]})
-            
+
             try:
-                st.write(response["Final Answer"])
+                st.write(json.loads(response["output"])[0]["Final Answer"])
             except:
                 st.write(response["output"])
 
             try:
-                st.plotly_chart(response["plot"])
+                st.plotly_chart(json.loads(response["output"])[0]["plot"])
             except:
                 print(None)
 >>>>>>> f083876 (output parsing, instruction updates)
